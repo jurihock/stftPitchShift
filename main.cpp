9cae0437bdddd8b14b0796a5e745ba33b7c3630d
Juergen Hock <juergen.hock@jurihock.de>
2022-03-28 23:10:34 +0200

    Add command line arguments

--- main.cpp
+++ main.cpp
@@ -1,70 +1,149 @@
 #include <iostream>
 
+#include <anyoption/anyoption.h>
+
 #include <IO.h>
 #include <STFT.h>
 #include <Vocoder.h>
 
 #include <smb/smbPitchShift.h>
 
-int main()
+int main(int argc, char** argv)
 {
-  const std::string infile = "voice.wav";
-  const std::string outfile = "test.wav";
+  AnyOption args;
 
-  const float factor = 0.5;
+  args.addUsage("Usage:");
+  args.addUsage("");
+  args.addUsage("-h,?  --help     print this help");
+  args.addUsage("");
+  args.addUsage("-i    --input    input .wav file name");
+  args.addUsage("-o    --output   output .wav file name");
+  args.addUsage("");
+  args.addUsage("-p    --pitch    pitch shifting factor");
+  args.addUsage("                 (default 1.0)");
+  args.addUsage("");
+  args.addUsage("-w    --window   sfft window size");
+  args.addUsage("                 (default 1024)");
+  args.addUsage("");
+  args.addUsage("-v    --overlap  stft window overlap");
+  args.addUsage("                 (default 32)");
+  args.addUsage("");
+  args.addUsage("      --smb      enable original algorithm");
 
-  const int framesize = 1 * 1024;
-  const int hoprate = 32;
-  const int hopsize = framesize / hoprate;
+  args.setFlag("help");
+  args.setFlag('h');
+  args.setFlag('?');
+  args.setFlag("smb");
+  args.setOption("input", 'i');
+  args.setOption("output", 'o');
+  args.setOption("pitch", 'p');
+  args.setOption("window", 'w');
+  args.setOption("overlap", 'v');
 
-  float samplerate;
+  args.processCommandArgs(argc, argv);
 
-  std::vector<float> indata, outdata;
-
-  IO::read(infile, indata, samplerate);
-
-  outdata.resize(indata.size());
-
-  if (true)
+  if (!args.hasOptions() || args.getFlag("help") || args.getFlag('h') || args.getFlag('?'))
   {
-    STFT stft(framesize, hopsize);
-    Vocoder vocoder(framesize, hopsize, samplerate);
+    args.printUsage();
+    return 0;
+  }
 
-    stft(indata, outdata, [&](std::vector<std::complex<float>>& src)
+  bool smb = false;
+
+  std::string infile = "";
+  std::string outfile = "";
+
+  float factor = 1.0;
+
+  int framesize = 1024;
+  int hoprate = 32;
+
+  if (args.getFlag("smb"))
+  {
+    smb = true;
+  }
+
+  if (args.getValue("input") || args.getValue('i'))
+  {
+    infile = args.getValue("input");
+  }
+
+  if (args.getValue("output") || args.getValue('o'))
+  {
+    outfile = args.getValue("output");
+  }
+
+  if (args.getValue("pitch") || args.getValue('p'))
+  {
+    factor = std::stof(args.getValue("pitch"));
+  }
+
+  if (args.getValue("window") || args.getValue('w'))
+  {
+    framesize = std::stoi(args.getValue("window"));
+  }
+
+  if (args.getValue("overlap") || args.getValue('v'))
+  {
+    hoprate = std::stoi(args.getValue("overlap"));
+  } 
+
+  try
+  {
+    float samplerate;
+
+    std::vector<float> indata, outdata;
+
+    IO::read(infile, indata, samplerate);
+
+    outdata.resize(indata.size());
+
+    if (smb)
     {
-      std::vector<std::complex<float>> dst(src.size());
+      smbPitchShift(
+        factor,
+        indata.size(),
+        framesize,
+        hoprate,
+        samplerate,
+        indata.data(),
+        outdata.data());
+    }
+    else
+    {
+      STFT stft(framesize, framesize / hoprate);
+      Vocoder vocoder(framesize, framesize / hoprate, samplerate);
 
-      vocoder.encode(src);
-
-      for (size_t i = 0; i < src.size(); ++i)
+      stft(indata, outdata, [&](std::vector<std::complex<float>>& src)
       {
-        const size_t j = i * factor;
+        std::vector<std::complex<float>> dst(src.size());
 
-        if (j >= src.size())
-          continue;
+        vocoder.encode(src);
 
-        dst[j].real(dst[j].real() + std::real(src[i]));
-        dst[j].imag(std::imag(src[i]) * factor);
-      }
+        for (size_t i = 0; i < src.size(); ++i)
+        {
+          const size_t j = i * factor;
 
-      vocoder.decode(dst);
+          if (j >= src.size())
+            continue;
 
-      src = dst;
-    });
+          dst[j].real(dst[j].real() + std::real(src[i]));
+          dst[j].imag(std::imag(src[i]) * factor);
+        }
+
+        vocoder.decode(dst);
+
+        src = dst;
+      });
+    }
+
+    IO::write(outfile, outdata, samplerate);
   }
-  else
+  catch (const std::exception& exception)
   {
-    smbPitchShift(
-      factor,
-      indata.size(),
-      framesize,
-      hoprate,
-      samplerate,
-      indata.data(),
-      outdata.data());
+    std::cerr << exception.what() << std::endl;
+    return 1;
   }
-
-  IO::write(outfile, outdata, samplerate);
 
   return 0;
 }
