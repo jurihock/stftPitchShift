4b32276810e6d6bd759a355a8f41cc3acc7f461f
Juergen Hock <juergen.hock@jurihock.de>
2022-04-11 21:22:10 +0200

    Create Windows build & launch script

---
+++ stftpitchshift.bat
@@ -0,0 +1,13 @@
+@echo off
+
+mkdir build-release
+
+pushd build-release
+cmake -A x64 .. || exit /B 1
+cmake --build . --config Release || exit /B 1
+popd
+
+set example=examples\voice
+
+build-release\Release\stftpitchshift.exe -i %example%.wav -o %example%.cpp.wav %*
+python stftpitchshift.py -i %example%.wav -o %example%.python.wav %*
