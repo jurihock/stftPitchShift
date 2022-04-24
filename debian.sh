9f0067b62152b0a7f96cd0658283c865807c4c2d
Jürgen Hock <juergen.hock@jurihock.de>
2022-04-24 17:46:09 +0200

    Create debian test build script

---
+++ debian.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BUILD=build-debian
+VERSION=$(cat VERSION | xargs)
+
+rm -rf ${BUILD}
+
+cmake -S . -B ${BUILD} -DSHARED=ON -DDEB=ON
+cpack --config ${BUILD}/CPackSourceConfig.cmake
+
+tar -xzvf stftpitchshift_${VERSION}.tar.gz -C ${BUILD}
+
+pushd ${BUILD}/stftpitchshift_${VERSION}
+debuild -B -us -ui -uc
+popd
