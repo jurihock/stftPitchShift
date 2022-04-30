c806149028423b4edb194c3139bbb296c004e828
Jürgen Hock <juergen.hock@jurihock.de>
2022-04-30 15:04:16 +0200

    Create prototype of the 2N point FFT #7

---
+++ cpp/StftPitchShift/RFFT.h
@@ -0,0 +1,209 @@
+#pragma once
+
+#include <StftPitchShift/FFT.h>
+
+#include <pocketfft/pocketfft_hdronly.h>
+
+#include <cassert>
+
+template<typename T>
+class RFFT : public FFT
+{
+
+public:
+
+  void fft(const std::vector<T>& frame, std::vector<std::complex<T>>& dft) override
+  {
+    assert(dft.size() >= frame.size() / 2);
+
+    cache.resize(frame.size());
+
+    const size_t size = cache.size.half;
+
+    auto input = reinterpret_cast<const std::complex<T>*>(frame.data());
+    auto output = dft.data();
+    auto buffer = cache.buffer.data();
+    auto a = cache.a.data();
+    auto b = cache.b.data();
+
+    // transform(-1, reinterpret_cast<const std::complex<T>*>(input.data()), buffer.data(), N);
+
+    pocketfft::c2c(
+      { size },
+      { sizeof(std::complex<T>) },
+      { sizeof(std::complex<T>) },
+      { 0 },
+      true,
+      input,
+      buffer,
+      T(1));
+
+    unpack(buffer, output, a, b, size);
+
+    const T W = T(0.5) / T(size);
+
+    for (size_t i = 0; i < size; ++i)
+    {
+      output[i] *= W;
+    }
+  }
+
+  void ifft(const std::vector<std::complex<T>>& dft, std::vector<T>& frame) override
+  {
+    assert(frame.size() <= dft.size() * 2);
+
+    cache.resize(frame.size());
+
+    const size_t size = cache.size.half;
+
+    auto input = dft.data();
+    auto output = reinterpret_cast<std::complex<T>* const>(frame.data());
+    auto buffer = cache.buffer.data();
+    auto a = cache.a.data();
+    auto b = cache.b.data();
+
+    pack(input, buffer, a, b, size);
+
+    pocketfft::c2c(
+      { size },
+      { sizeof(std::complex<T>) },
+      { sizeof(std::complex<T>) },
+      { 0 },
+      false,
+      buffer,
+      output,
+      T(1));
+
+    // transform(+1, input.data(), reinterpret_cast<std::complex<T>*>(output.data()), N);
+  }
+
+private:
+
+  struct
+  {
+    struct
+    {
+      size_t full = 0;
+      size_t half = 0;
+    }
+    size;
+
+    std::vector<std::complex<T>> buffer;
+
+    std::vector<std::complex<T>> w, a, b;
+
+    void resize(size_t newsize)
+    {
+      if (size.full == newsize)
+      {
+        return;
+      }
+
+      size.full = newsize;
+      size.half = newsize / 2;
+
+      buffer.resize(size.half);
+
+      w.resize(size.half);
+      a.resize(size.half);
+      b.resize(size.half);
+
+      const T pi = T(-2) * std::acos(T(-1)) / T(size.full);
+
+      for (size_t i = 0; i < size.half; ++i)
+      {
+        w[i] = std::polar(T(1), pi * i);
+
+        a[i] = imul({ T(0.5), T(0.5) }, -w[i]);
+        b[i] = imul({ T(0.5), T(0.5) }, +w[i]);
+      }
+    }
+  }
+  cache;
+
+  static std::complex<T> imul(const std::complex<T>& x, const std::complex<T>& y)
+  {
+    return // x.r + j * x.i * (y.r + j * y.i)
+    {
+      x.real() - x.imag() * y.imag(),
+      x.imag() * y.real()
+    };
+  }
+
+  static void unpack(const std::complex<T>* input, std::complex<T>* const output, const std::complex<T>* a, const std::complex<T>* b, const size_t size)
+  {
+    output[0] = input[0] * a[0] + std::conj(input[0]) * b[0];
+
+    for (size_t i = 1, j = size - 1; i < size; ++i, --j)
+    {
+      output[i] = input[i] * a[i] + std::conj(input[j]) * b[i];
+    }
+  }
+
+  static void pack(const std::complex<T>* input, std::complex<T>* const output, const std::complex<T>* a, const std::complex<T>* b, const size_t size)
+  {
+    output[0] = input[0] * std::conj(a[0]) + std::conj(input[0]) * std::conj(b[0]);
+
+    for (size_t i = 1, j = size - 1; i < size; ++i, --j)
+    {
+      output[i] = input[i] * std::conj(a[i]) + std::conj(input[j]) * std::conj(b[i]);
+    }
+  }
+
+  // static size_t rebit(size_t x, size_t bits)
+  // {
+  //   size_t y = 0;
+
+  //   for (size_t i = 0; i < bits; ++i)
+  //   {
+  //     y <<= 1;
+  //     y |= (x & 1);
+  //     x >>= 1;
+  //   }
+
+  //   return y;
+  // }
+
+  // void transform(const T sign, const std::complex<T>* input, std::complex<T>* const output, const size_t size)
+  // {
+  //   const size_t bits = static_cast<size_t>(std::log2(size));
+
+  //   for (size_t i = 0; i < size; ++i)
+  //   {
+  //     output[rebit(i, bits)] = input[i];
+  //   }
+
+  //   std::vector<std::complex<T>> twiddles(size / 2);
+  //   {
+  //     const T pi = sign * T(2) * std::acos(T(-1)) / T(size);
+
+  //     for (size_t i = 0; i < twiddles.size(); ++i)
+  //     {
+  //       twiddles[i] = std::polar(T(1), pi * i);
+  //     }
+  //   }
+
+  //   for (size_t bit = 1; bit <= bits; ++bit)
+  //   {
+  //     const size_t n1 = 1 << bit;
+  //     const size_t n2 = n1 >> 1;
+
+  //     const size_t dt = size / n1;
+
+  //     for (size_t i = 0; i < size; i += n1)
+  //     {
+  //       for (size_t j = i, t = 0; j < i + n2; ++j, t += dt)
+  //       {
+  //         const auto left = output[j];
+  //         const auto right = output[j + n2] * twiddles[t];
+
+  //         output[j] = left + right;
+  //         output[j + n2] = left - right;
+  //       }
+  //     }
+
+  //     // if (n1 == size)
+  //     //   break;
+  //   }
+  // }
+};
