a1344b99cd43b89329bed0a2021e58946ee2cf19
Juergen Hock <juergen.hock@jurihock.de>
2022-05-02 18:33:18 +0200

    Replace np.interp with C++ approach
    the np.interp leads to some kind of amplitude modulation if -p <1

--- python/stftpitchshift/resampler.py
+++ python/stftpitchshift/resampler.py
@@ -3,17 +3,40 @@ import numpy as np
 
 def linear(x, factor):
 
+    if factor == 1:
+        return x.copy()
+
     N = len(x)
     M = round(N * factor)
 
-    y = np.interp(
-        np.linspace(0, 1, M, endpoint=False),
-        np.linspace(0, 1, N, endpoint=False),
-        x)
+    y = np.zeros(x.shape, dtype=x.dtype)
 
-    y.resize(x.shape)
+    for m in range(min(M, N)):
+    
+        n = m / factor
+
+        n0 = int(np.floor(n))
+        n1 = int(np.ceil(n))
+        
+        if n0 < 0 or N <= n0:
+            continue
+
+        if n1 < 0 or N <= n1:
+            continue
+
+        if n0 == n1:
+            y[m] = x[n0]
+            continue
+        
+        y0 = x[n0]
+        y1 = x[n1]
+
+        i = (n - n0) / (n1 - n0)
+        
+        y[m] = y0 * (1 - i) + y1 * i
 
     return y
+
 
 def bilinear(x0, x1, factor):
 
