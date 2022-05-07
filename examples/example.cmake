9cc994eef2830b972c11740c93b29991de9bbf82
Jürgen Hock <juergen.hock@jurihock.de>
2022-05-07 14:05:08 +0200

    Update example cmake

--- examples/example.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.1...3.18)
-
-project(example)
-
-# don't forget to include LibStftPitchShift.cmake
-
-add_executable(example "${CMAKE_CURRENT_LIST_DIR}/example.cpp")
-target_link_libraries(example LibStftPitchShift)
-target_compile_features(example PRIVATE cxx_std_11)
