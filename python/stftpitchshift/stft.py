e00a9c020419bbd897fc34f8778a2e1f43b3ee53
Jürgen Hock <juergen.hock@jurihock.de>
2023-11-12 12:23:55 +0100

    Implement asymmetric windows #38

--- python/stftpitchshift/stft.py
+++ python/stftpitchshift/stft.py
@@ -3,42 +3,119 @@ from numpy.lib.stride_tricks import sliding_window_view
 import numpy as np
 
 
-def stft(x, framesize, hopsize):
+def stft(x, framesize, hopsize, dftsize=None):
 
-    frames = sliding_window_view(x, framesize, writeable=False)[::hopsize]
+    # check input
 
-    M, N = frames.shape
+    x = np.atleast_1d(x)
 
-    data = np.zeros((M, N//2+1), complex)
+    assert x.ndim == 1, f'Expected 1D array (samples,), got {x.shape}!'
 
-    w = 0.5 - 0.5 * np.cos(2 * np.pi * np.arange(framesize) / framesize)
+    # construct window
 
-    for i, frame in enumerate(frames):
+    analysis_window_size = framesize if dftsize is None else \
+                           np.fft.irfft([0] * dftsize).size
 
-        data[i] = np.fft.rfft(w * frame, norm='forward')
+    synthesis_window_size = framesize
 
-        # keep dc and nyquist as is
-        # data[i][0] = data[i][-1] = 0
+    assert analysis_window_size >= synthesis_window_size, \
+        f'Invalid framesize and dftsize combination!'
 
-    return data
+    W = asymmetric_analysis_window(analysis_window_size, synthesis_window_size) \
+        if analysis_window_size != synthesis_window_size else \
+        symmetric_window(analysis_window_size)
+
+    # perform analysis
+
+    frames0 = sliding_window_view(x, analysis_window_size, writeable=False)[::hopsize]
+    frames1 = np.fft.rfft(frames0 * W, axis=-1, norm='forward')
+
+    # optionally zero dc and nyquist
+    if False:
+        frames1[:,  0] = 0
+        frames1[:, -1] = 0
+
+    return frames1
 
 
-def istft(frames, framesize, hopsize):
+def istft(frames, framesize, hopsize, dftsize=None):
 
-    M, N = frames.shape
+    # check input
 
-    y = np.zeros((M * hopsize + framesize), float)
+    frames = np.atleast_2d(frames)
 
-    data = sliding_window_view(y, framesize, writeable=True)[::hopsize]
+    assert frames.ndim == 2, f'Expected 2D array (samples,frequencies), got {frames.shape}!'
 
-    w = 0.5 - 0.5 * np.cos(2 * np.pi * np.arange(framesize) / framesize)
-    w *= hopsize / np.sum(w**2)
+    # construct window
 
-    for i, frame in enumerate(frames):
+    analysis_window_size = framesize if dftsize is None else \
+                           np.fft.irfft([0] * dftsize).size
 
-        # zero dc and nyquist
-        frame[0] = frame[-1] = 0
+    synthesis_window_size = framesize
 
-        data[i] += w * np.fft.irfft(frame, norm='forward')
+    assert analysis_window_size >= synthesis_window_size, \
+        f'Invalid framesize and dftsize combination!'
+
+    A = asymmetric_analysis_window(analysis_window_size, synthesis_window_size) \
+        if analysis_window_size != synthesis_window_size else \
+        symmetric_window(analysis_window_size)
+
+    S = asymmetric_synthesis_window(analysis_window_size, synthesis_window_size) \
+        if analysis_window_size != synthesis_window_size else \
+        symmetric_window(analysis_window_size)
+
+    W = S * hopsize / np.sum(A * S)
+
+    # perform synthesis
+
+    N = frames.shape[0] * hopsize + analysis_window_size
+
+    y = np.zeros((N), float)
+
+    # optionally zero dc and nyquist
+    if True:
+        frames[:,  0] = 0
+        frames[:, -1] = 0
+
+    frames0 = sliding_window_view(y, analysis_window_size, writeable=True)[::hopsize]
+    frames1 = np.fft.irfft(frames, axis=-1, norm='forward') * W
+
+    for i in range(min(len(frames0), len(frames1))):
+        frames0[i] += frames1[i]
 
     return y
+
+
+def symmetric_window(n):
+
+    return 0.5 - 0.5 * np.cos(2 * np.pi * np.arange(n) / n)
+
+
+def asymmetric_analysis_window(n, m):
+
+    m //= 2
+
+    left = symmetric_window(2 * n - 2 * m)
+    right = symmetric_window(2 * m)
+
+    weights = np.zeros(n)
+
+    weights[:n-m] = left[:n-m]
+    weights[-m:] = right[-m:]
+
+    return weights
+
+
+def asymmetric_synthesis_window(n, m):
+
+    m //= 2
+
+    left = symmetric_window(2 * n - 2 * m)
+    right = symmetric_window(2 * m)
+
+    weights = np.zeros(n)
+
+    weights[n-m-m:n-m] = right[:m] / left[n-m-m:n-m]
+    weights[-m:] = right[-m:]
+
+    return weights
