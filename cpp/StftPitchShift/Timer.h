3c30eb691e1571779bce969fb0fe18479366c93c
Jürgen Hock <juergen.hock@jurihock.de>
2023-11-21 20:05:23 +0100

    Revert "Update Timer.h"
    This reverts commit d96608955dfbb75e85f8a78af4ed77e0299ed736.

--- cpp/StftPitchShift/Timer.h
+++ cpp/StftPitchShift/Timer.h
@@ -35,65 +35,26 @@ namespace stftpitchshift
       data.assign(other.data.begin(), other.data.end());
     }
 
-    size_t capacity() const
-    {
-      return data.capacity();
-    }
-
-    size_t size() const
-    {
-      return data.size();
-    }
-
     void cls()
     {
       data.clear();
     }
 
-    double lap() const
-    {
-      if (data.empty())
-      {
-        return 0;
-      }
-
-      return data.front();
-    }
-
     void tic()
     {
       timestamp = std::chrono::steady_clock::now();
-
-      if (data.empty())
-      {
-        data.push_back(0);
-      }
     }
 
     void toc()
     {
-      if (data.empty())
-      {
-        tic();
-      }
-
       const std::chrono::steady_clock::duration duration = std::chrono::steady_clock::now() - timestamp;
       const double value = std::chrono::duration_cast<T>(duration * 1e+3).count() * 1e-3;
-
-      data.front() += value;
 
       data.push_back(value);
     }
 
     std::string str()
     {
-      const size_t offset = 1;
-
-      if (data.size() <= offset)
-      {
-        return "";
-      }
-
       const std::map<intmax_t, std::string> units =
       {
         { 1000000000, "ns" },
@@ -104,20 +65,15 @@ namespace stftpitchshift
 
       const std::string unit = units.at(T::period::num * T::period::den);
 
-      const size_t n = data.size() - offset;
+      const double sum = std::accumulate(data.begin(), data.end(), 0.0);
+      const double sumsum = std::inner_product(data.begin(), data.end(), data.begin(), 0.0);
 
-      const double sum = std::accumulate(
-        data.begin() + offset, data.end(), 0.0);
-
-      const double sumsum = std::inner_product(
-        data.begin() + offset, data.end(), data.begin() + offset, 0.0);
-
-      const double mean = sum / n;
-      const double stdev = std::sqrt(sumsum / n - mean * mean);
+      const double mean = sum / data.size();
+      const double stdev = std::sqrt(sumsum / data.size() - mean * mean);
 
       std::ostringstream result;
       result.precision(3);
-      result << mean << " ± " << stdev << " " << unit << " n=" << n;
+      result << mean << " ± " << stdev << " " << unit << " n=" << data.size();
 
       return result.str();
     }
